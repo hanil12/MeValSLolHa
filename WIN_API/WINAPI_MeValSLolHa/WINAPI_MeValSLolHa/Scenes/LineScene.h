c0d3772219d1c8cf48edd505651e68d2ac729120
hanil12 <ekdrhaod@gmail.com>
2024-12-06 21:47:55 +0900

    LineScene

--- WIN_API/WINAPI_MeValSLolHa/WINAPI_MeValSLolHa/Scenes/LineScene.h
+++ WIN_API/WINAPI_MeValSLolHa/WINAPI_MeValSLolHa/Scenes/LineScene.h
@@ -2,10 +2,16 @@
 class LineScene : public Scene
 {
 public:
+	LineScene();
+	~LineScene();
+
 	// Scene을(를) 통해 상속됨
 	void Update() override;
 	void Render(HDC hdc) override;
 
 private:
+	shared_ptr<Line> _line1;
+	shared_ptr<Line> _line2;
+	shared_ptr<Line> _floor;
 };
 
