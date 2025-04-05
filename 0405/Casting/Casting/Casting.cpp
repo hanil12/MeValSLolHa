1da894d3232f312d46ad87db7f03fc851c56ba5f
hanil12 <ekdrhaod@gmail.com>
2025-04-05 14:20:35 +0900

    DeepCopy

---
+++ 0405/Casting/Casting/Casting.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// 1. UP Casting, DOWN Casting
+// 2. C++ Ä³½ºÆÃ 4ÃÑ»ç
+// - static cast
+// - dynamic cast
+// - const cast
+// - reinterpret cast
+
+int main()
+{
+
+	return 0;
+}
---
+++ 0405/Casting/Casting/DeepCopy.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Pet
+{
+public:
+	Pet() { cout << "Pet 생성" << endl; }
+	Pet(const Pet& other) { cout << "Pet의 복사 생성" << endl; }
+	~Pet() { cout << "Pet 소멸" << endl; }
+};
+
+class Player
+{
+public:
+	// 기본생성자 : 암묵적으로 만들어줌
+	Player()
+	{
+		_pet = new Pet();
+	}
+
+	// 복사생성자 : 암묵적으로 만들어줌 => 얕은 복사 생성자
+	// 깊은 복사 : 내부에서 생성하고 지울 때, 복사생성자도 같이 생성해줘야한다.
+	Player(const Player& other)
+	{
+		_pet = new Pet(*other._pet); // 깊은 복사
+	}
+
+	~Player()
+	{
+		delete _pet;
+	}
+
+private:
+	Pet* _pet;
+};
+
+
+int main()
+{
+	Player* p = new Player();
+	Player* p2 = new Player(*p);
+
+	delete p;
+	delete p2;
+
+	return 0;
+}
